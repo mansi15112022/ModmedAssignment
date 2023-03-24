e70d8df2b18857cfda719c47caca06c7f6c1e81c
mansi.sahu <mansi.sahu@3pillarglobal.com>
2023-03-24 20:30:28 +0530

    Added leap year logic

--- src/main/java/javaDemo/AssignmentRomaniaADV.java
+++ src/main/java/javaDemo/AssignmentRomaniaADV.java
@@ -7,9 +7,9 @@ public class AssignmentRomaniaADV {
 	public static void main(String[] args) {
 		int sum;
 		Scanner sc = new Scanner(System.in);
-		int[] number = new int[1];
+		int[] number = new int[5];
 
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 5; i++) {
 			number[i] = sc.nextInt();
 		}
 
